374ec9db3d9ca31a9500fe004800ffd2acf5ff8b
Shang, Ying <ying.shang@news.com.au>
2018-12-18 17:18:27 +1100

    back to initial result

--- js/custom.js
+++ js/custom.js
@@ -33,25 +33,3 @@ jssor_1_slider_init = function () {
     $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
     /*#endregion responsive code end*/
 };
-
-/**
- * Lazy-loading background image
- */
-document.addEventListener("DOMContentLoaded", function () {
-    var lazyBackgrounds = [].slice.call(document.querySelectorAll(".lazy-s2, .lazy-s3, .parallax-window"));
-
-    if ("IntersectionObserver" in window) {
-        let lazyBackgroundObserver = new IntersectionObserver(function (entries, observer) {
-            entries.forEach(function (entry) {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add("visible");
-                    lazyBackgroundObserver.unobserve(entry.target);
-                }
-            });
-        });
-
-        lazyBackgrounds.forEach(function (lazyBackground) {
-            lazyBackgroundObserver.observe(lazyBackground);
-        });
-    }
-});
